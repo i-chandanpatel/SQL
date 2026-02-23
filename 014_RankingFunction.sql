42bcf04d895d7dc68f62987bb973c217edae6167
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2026-02-23 12:01:56 +0530

    Update 014_RankingFunction.sql

--- 014_RankingFunction.sql
+++ 014_RankingFunction.sql
@@ -95,3 +95,40 @@ OrderID | ProductID | Sales | SalesRank
 1       | 101       | 10    | 10
 */
 
+
+/*==============================================================
+DENSE_RANK()
+
+Assigns same rank to ties.
+Does NOT leave gaps in ranking.
+
+Example:
+If two rows tie for rank 2,
+the next rank will be 3.
+
+Use Case:
+• Business ranking where gaps are not desired
+• Performance tier classification
+==============================================================*/
+
+SELECT 
+    OrderID,
+    ProductID,
+    Sales,
+    DENSE_RANK() OVER (ORDER BY Sales DESC) AS SalesRank
+FROM Sales.Orders;
+
+/*
+OrderID | ProductID | Sales | SalesRank
+8       | 101       | 90    | 1
+4       | 105       | 60    | 2
+10      | 102       | 60    | 2
+6       | 104       | 50    | 3
+7       | 102       | 30    | 4
+5       | 104       | 25    | 5
+9       | 101       | 20    | 6
+3       | 101       | 20    | 6
+2       | 102       | 15    | 7
+1       | 101       | 10    | 8
+*/
+
